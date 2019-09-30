9b21a0a21417a11badd211581791435825bb906b
sirius206 <46640143+sirius206@users.noreply.github.com>
2019-09-30 11:42:00 -0400

    Easy: Array, Math, Binary search, two pointers

--- 167-TwoSumII-InputArraySorted.java
+++ 167-TwoSumII-InputArraySorted.java
@@ -24,4 +24,17 @@ class Solution {
 
 //-----------------------------------------------------------
 //method 2: two pointers
-
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int lo = 0;
+        int hi = numbers.length -1;
+        while (lo <= hi) {
+            int sum = numbers[lo] + numbers[hi];
+            if (sum > target) hi--;
+            else if (sum < target) lo++;
+            else return new int[]{lo+1, hi+1};
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    
+}
