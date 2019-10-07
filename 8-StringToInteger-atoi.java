18c4617c41a9f509a7147ea69814d08f0a3a4df6
sirius206 <46640143+sirius206@users.noreply.github.com>
2019-10-06 21:35:21 -0400

    Medium：Math, Max int, overflow, String

--- 8-StringToInteger-atoi.java
+++ 8-StringToInteger-atoi.java
@@ -1,21 +1,23 @@
-// wrong
+// wrong Not sure why LC has index out of bounds error, IDE doesn't
 //package edu.gatech.seclass;
 
 class Solution {
     public int myAtoi(String str) {
-        if (str.equals("")) return 0;
         String [] strAr = str.trim().split(" +");
         char[] strChar = strAr[0].toCharArray();
+        if (strChar.length == 0) return 0;
         char first = strChar[0];
         int len = strChar.length;
+        int sign = 1;
+        if (first == '-') sign = -1;
         if (first != '+' && first != '-'
                 && !(first >= '0' && first <= '9')) {
             return 0;
         }
         else {
             int x = 0;
-            int max = (int) Math.pow(2, 31) - 1;
-            int min = ((int) Math.pow(2, 31))*(-1) - 1;
+            int max = Integer.MAX_VALUE;
+            int min = Integer.MIN_VALUE;
             int a;
             if (first == '+' || first == '-'){
                 a = 1;
@@ -23,18 +25,18 @@ class Solution {
             else a = 0;
             for (int i = a; i < len; i++){
                 if (!(strChar[i] >= '0' && strChar[i] <= '9')){
-                    return x;
+                    return x * sign;
                 }
-                if (first == '-' && (x > (min * (-1) - Character.getNumericValue(strChar[i]))/10)) {
+// /10 时余数舍掉了 所以转成double比较大小
+                if (first == '-' && ((double) x * (-1) <= (double) (min + Character.getNumericValue(strChar[i]))/10)) {
                     return min;
                 }
-                else if (x > (max - Character.getNumericValue(strChar[i]))/10){
+                else if ((double) x > (double) (max - Character.getNumericValue(strChar[i]))/10){
                     return max;
                 }
                 x = 10 * x + Character.getNumericValue(strChar[i]);
             }
-            if (first == '-') return x * (-1);
-            else return x;
+            return x * sign;
         }
     }
 }
