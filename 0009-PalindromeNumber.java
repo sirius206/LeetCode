0d274093a3c6b2e06233cc05d62c9ba1d5282d0a
sirius206 <46640143+sirius206@users.noreply.github.com>
2019-10-11 14:38:03 -0400

    Easy: math, two pointers

--- 0009-PalindromeNumber.java
+++ 0009-PalindromeNumber.java
@@ -1,5 +1,54 @@
 //
-//Method 1: two pointers 
+//Method 1 by my self, compares the two digits from each end
+//slow
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+        int numdigit = 0;
+        int y = x;
+        while (y > 0) {
+            y = y /10;
+            numdigit++;
+        }
+        for (int i = 1; i <= numdigit/2; i++) {
+            int a = (int)(x / Math.pow(10, (numdigit - i))) - (int)(x / Math.pow(10, (numdigit - i +1))) * 10;
+            int m = (int)(x % Math.pow(10, i));
+            int n = (int)(x % Math.pow(10, i - 1));
+            int b = (m - n ) / ((int) Math.pow(10, i - 1));
+            if (a != b)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+//Method 2 from book, compare two ends, then chop the two ends
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+        int div = 1;
+        while (x / div >= 10) {
+            div = div * 10;
+        }
+        while (x > 0) {
+            if (x / div != x % 10) {
+                return false;
+            }
+            else {
+                x = (x % div) / 10;
+                div = div / 100;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+//Method 3: two pointers 
 //Need extra space
 //run time: O(n) Space: o(logn)
 class Solution {
