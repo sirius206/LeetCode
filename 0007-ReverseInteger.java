86de63a6be5a8d6b33c329d4c5add95fce11b875
sirius206 <46640143+sirius206@users.noreply.github.com>
2019-10-11 10:56:10 -0400

    Easy: Math, overflow

--- 0007-ReverseInteger.java
+++ 0007-ReverseInteger.java
@@ -1,4 +1,21 @@
-//Method 1 wrong
+//
+
+class Solution {
+    public int reverse(int x) {
+        int rInt = 0;
+//        while (Math.abs(x) > 0) {
+        while (x != 0) {            
+            if (Math.abs (rInt) > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            rInt = x % 10 + rInt * 10;
+            x = x / 10;
+        }
+    return rInt;
+    }
+}
+
+//Method 2 wrong
 
 class Solution {
     public int reverse(int x) {
