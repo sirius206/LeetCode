159bf9f1cb3741f25b4c3eecbc285b0aa7994b89
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-02-26 22:19:57 -0500

    Update 0349-Intersection_of_Two_Arrays.java

--- 0349-Intersection_of_Two_Arrays.java
+++ 0349-Intersection_of_Two_Arrays.java
@@ -34,27 +34,6 @@ public:
     }
 };
 
-//4 Two sets
-class Solution {
-  public int[] set_intersection(HashSet<Integer> set1, HashSet<Integer> set2) {
-    int [] output = new int[set1.size()];
-    int idx = 0;
-    for (Integer s : set1)
-      if (set2.contains(s)) output[idx++] = s;
-
-    return Arrays.copyOf(output, idx);
-  }
-
-  public int[] intersection(int[] nums1, int[] nums2) {
-    HashSet<Integer> set1 = new HashSet<Integer>();
-    for (Integer n : nums1) set1.add(n);
-    HashSet<Integer> set2 = new HashSet<Integer>();
-    for (Integer n : nums2) set2.add(n);
-
-    if (set1.size() < set2.size()) return set_intersection(set1, set2);
-    else return set_intersection(set2, set1);
-  }
-}
 
 // 3 sort one array then Binary Search Time O(m log m + m log n ) Space O(1)
 class Solution {
