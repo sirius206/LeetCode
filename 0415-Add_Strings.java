d3740a08b9ba1cd1fc4ca7307c2597d368617067
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-02-28 21:36:04 -0500

    Update 0415-Add_Strings.java

--- 0415-Add_Strings.java
+++ 0415-Add_Strings.java
@@ -2,30 +2,29 @@
 // Time O(m+n) Space O(1)
 class Solution {
     public String addStrings(String num1, String num2) {
+        int sum = 0;
+        int carry = 0;
+        int value = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-        int carry = 0;
-        int sum = 0; 
-        String res = "";
         int a = 0;
         int b = 0;
-        int value = 0;
+        String res = "";
         while (i >= 0 || j >= 0) {
-            if (i >= 0)  a = num1.charAt(i) - '0'; 
+            if (i >= 0) a = num1.charAt(i) - '0';
             else a = 0;
-            if (j >= 0)  b = num2.charAt(j) - '0'; 
-            else b = 0; 
+            if (j >= 0) b = num2.charAt(j) - '0';
+            else b = 0;            
             sum = a + b + carry;
             carry = sum / 10;
             value = sum % 10;
-            res = Integer.toString(value) + res;
+            res = value + res;
             i--;
             j--;
         }
-        if (carry == 1) {
-            res = "1" + res;
-        }
+        if  (carry == 1) res = "1" + res;
         return res;
+        
     }
 }
 
