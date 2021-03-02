a47a90e7a4d3dce68644a328f65293e9ec83a932
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-01 21:54:25 -0500

    Create 404-Sum_of_Left_Leaves.java

---
+++ 404-Sum_of_Left_Leaves.java
@@ -0,0 +1,9 @@
+//DFS Time O(N), Space: Completely balanced Olog(N), completely unbalanced O(n)
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) return root.left.val + sumOfLeftLeaves(root.right);
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
