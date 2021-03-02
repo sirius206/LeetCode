656a87e544294837742f79994497901ccfa3bb6b
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-01 21:24:51 -0500

    Create 0111-Minimum_Depth_of_Binary_Tree.java

---
+++ 0111-Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,11 @@
+//DFS recursion
+//Time O(N), Space: Completely balanced Olog(N), completely unbalanced O(n)
+
+class Solution {
+public int minDepth(TreeNode root) {
+	if(root == null){return 0;}
+	if(root.left == null){return minDepth(root.right) + 1;}
+	if(root.right == null){return minDepth(root.left) + 1;}
+	return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
