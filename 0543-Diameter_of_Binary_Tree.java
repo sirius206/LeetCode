0ac66ace234316a09a68d3dcc40958f37eb896e8
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-02 22:09:39 -0500

    Create 0543-Diameter_of_Binary_Tree.java

---
+++ 0543-Diameter_of_Binary_Tree.java
@@ -0,0 +1,21 @@
+//Time O(n), Space O(n)
+//Let's calculate the depth of a node in the usual way: max(depth of node.left, depth of node.right) + 1. 
+//While we do, a path "through" this node uses 1 + (depth of node.left) + (depth of node.right) nodes. 
+//Let's search each node and remember the highest number of nodes used in some path. The desired length is 1 minus this number.
+
+class Solution {
+    int max;  // important to declare here so both functions can modify it
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        max = 1;   // 1, not 0, refers to height
+        depth(root);  // calculate max
+        return max - 1;  // -1 diameter
+    }
+    private int depth (TreeNode root){
+        if (root == null) return 0;
+        int L = depth(root.left);   // calculate before as the it's used twice later
+        int R = depth(root.right);
+        max = Math.max(max, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
+}
