63971481dfb8ca9ba6035f3b652517019df242df
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-05 21:05:05 -0500

    Create 0344-Reverse_String.java

---
+++ 0344-Reverse_String.java
@@ -0,0 +1,29 @@
+//1. Two pointers, Time O(n), Space O(1) 
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
+
+//2. Recursion Time O(n), Space O(n) 
+class Solution {
+  public void helper(char[] s, int left, int right) {
+    if (left >= right) return;
+    char tmp = s[left];
+    s[left++] = s[right];
+    s[right--] = tmp;
+    helper(s, left, right);
+  }
+
+  public void reverseString(char[] s) {
+    helper(s, 0, s.length - 1);
+  }
+}
