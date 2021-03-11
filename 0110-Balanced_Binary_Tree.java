d178403855992dbd909087a06996cabb56908ea1
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-10 22:29:34 -0500

    Create 0110-Balanced_Binary_Tree.java

---
+++ 0110-Balanced_Binary_Tree.java
@@ -0,0 +1,58 @@
+// 1. my solution Top-down recursion, redundant, Time O(n), Space O(n)
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        if (Math.abs(height(root.left) - height(root.right)) > 1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    private int height(TreeNode root) {
+        if (root == null) return 0;      // -1????
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+}
+
+//2: Bottom-up recursion 
+// Utility class to store information from recursive calls
+final class TreeInfo {
+  public final int height;
+  public final boolean balanced;
+
+  public TreeInfo(int height, boolean balanced) {
+    this.height = height;
+    this.balanced = balanced;
+  }
+}
+
+class Solution {
+  // Return whether or not the tree at root is balanced while also storing
+  // the tree's height in a reference variable.
+  private TreeInfo isBalancedTreeHelper(TreeNode root) {
+    // An empty tree is balanced and has height = -1
+    if (root == null) {
+      return new TreeInfo(-1, true);
+    }
+
+    // Check subtrees to see if they are balanced.
+    TreeInfo left = isBalancedTreeHelper(root.left);
+    if (!left.balanced) {
+      return new TreeInfo(-1, false);
+    }
+    TreeInfo right = isBalancedTreeHelper(root.right);
+    if (!right.balanced) {
+      return new TreeInfo(-1, false);
+    }
+
+    // Use the height obtained from the recursive calls to
+    // determine if the current node is also balanced.
+    if (Math.abs(left.height - right.height) < 2) {
+      return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+    }
+    return new TreeInfo(-1, false);
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    return isBalancedTreeHelper(root).balanced;
+  }
+};
