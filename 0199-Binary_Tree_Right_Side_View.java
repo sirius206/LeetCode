64a2c41f27e6bc7c6f4f47cc676a146ba73013fe
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-13 22:55:29 -0500

    Create 0199-Binary_Tree_Right_Side_View.java

---
+++ 0199-Binary_Tree_Right_Side_View.java
@@ -0,0 +1,45 @@
+//BFS Time O(n), Space O(D) 
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        return res;        
+    }
+}
+
+//2. DFS Time O(n), Space O(H) 
+//traverse the tree level by level, starting each time from the rightmost child (in order, but right first?)
+class Solution {
+    List<Integer> rightside = new ArrayList();
+    
+    public void helper(TreeNode node, int level) {
+        if (level == rightside.size()) 
+            rightside.add(node.val);
+        
+        if (node.right != null) 
+            helper(node.right, level + 1);  
+        if (node.left != null) 
+            helper(node.left, level + 1);
+    }    
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return rightside;
+        
+        helper(root, 0);
+        return rightside;
+    }
+}
