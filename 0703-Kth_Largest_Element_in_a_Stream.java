bd58890df6c8ef17d172aa38ee05374ce5b99238
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-15 19:40:02 -0400

    Create 0703-Kth_Largest_Element_in_a_Stream.java

---
+++ 0703-Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,46 @@
+//1. min Heap, Time O(n), Space O(k) 
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> minPQ;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minPQ = new PriorityQueue<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            minPQ.offer(nums[i]);
+            if (minPQ.size() > k) minPQ.poll();
+        }
+    }
+    
+    public int add(int val) {
+        minPQ.offer(val);
+        if (minPQ.size() > k) {
+            minPQ.poll();
+        }
+        return minPQ.peek();
+    }
+}
+
+
+//2. C++ multi Set
+class KthLargest {
+public:
+    KthLargest(int k, vector<int> nums) {
+        for (int num : nums) {
+            st.insert(num);
+            if (st.size() > k) st.erase(st.begin());
+        }
+        K = k;
+    }
+    
+    int add(int val) {
+        st.insert(val);
+        if (st.size() > K) st.erase(st.begin());
+        return *st.begin();
+    }
+
+private:
+    multiset<int> st;
+    int K;
+};
