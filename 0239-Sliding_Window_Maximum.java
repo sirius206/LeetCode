60587a059ecd55f37e7a73c9facc856873cab94a
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-16 23:04:52 -0400

    Create 0239-Sliding_Window_Maximum.java

---
+++ 0239-Sliding_Window_Maximum.java
@@ -0,0 +1,95 @@
+
+// 1. Heap with pair, slow, Time O(n logk) , Space O(k)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        PriorityQueue<Pair<Integer,Integer> > queue=
+                new PriorityQueue<Pair<Integer,Integer>>((a,b) -> b.getKey() - a.getKey());
+        for (int i = 0; i < len; i++) {
+            if (i >= k) {
+                while (queue.size() != 0 && (queue.peek().getValue() <= i - k))
+                    queue.poll();
+            }
+            queue.offer(new Pair(nums[i], i));
+            if (i >= k - 1) {
+                Pair<Integer, Integer> p1 = queue.peek();
+                res[i - k + 1] = p1.getKey();
+            }
+        }
+        return res;
+    }
+}
+
+//2. Double ended queue, Time O(n) , Space O(k)
+class Solution {
+  ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+  int [] nums;
+
+  public void clean_deque(int i, int k) {
+    // remove indexes of elements not from sliding window
+    if (!deq.isEmpty() && deq.getFirst() == i - k)
+      deq.removeFirst();
+
+    // remove from deq indexes of all elements 
+    // which are smaller than current element nums[i]
+    while (!deq.isEmpty() && nums[i] > nums[deq.getLast()])                           deq.removeLast();
+  }
+
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    int n = nums.length;
+    if (n * k == 0) return new int[0];
+    if (k == 1) return nums;
+
+    // init deque and output
+    this.nums = nums;
+    int max_idx = 0;
+    for (int i = 0; i < k; i++) {
+      clean_deque(i, k);
+      deq.addLast(i);
+      // compute max in nums[:k]
+      if (nums[i] > nums[max_idx]) max_idx = i;
+    }
+    int [] output = new int[n - k + 1];
+    output[0] = nums[max_idx];
+
+    // build output
+    for (int i  = k; i < n; i++) {
+      clean_deque(i, k);
+      deq.addLast(i);
+      output[i - k + 1] = nums[deq.getFirst()];
+    }
+    return output;
+  }
+}
+
+//3, DP, Time O(n) , Space O(N)
+class Solution {
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    int n = nums.length;
+    if (n * k == 0) return new int[0];
+    if (k == 1) return nums;
+
+    int [] left = new int[n];
+    left[0] = nums[0];
+    int [] right = new int[n];
+    right[n - 1] = nums[n - 1];
+    for (int i = 1; i < n; i++) {
+      // from left to right
+      if (i % k == 0) left[i] = nums[i];  // block_start
+      else left[i] = Math.max(left[i - 1], nums[i]);
+
+      // from right to left
+      int j = n - i - 1;
+      if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
+      else right[j] = Math.max(right[j + 1], nums[j]);
+    }
+
+    int [] output = new int[n - k + 1];
+    for (int i = 0; i < n - k + 1; i++)
+      output[i] = Math.max(left[i + k - 1], right[i]);
+
+    return output;
+  }
+}
+
