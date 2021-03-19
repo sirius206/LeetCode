be8c71001548f7bbf72e92daac9827857bed23f4
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-19 15:57:38 -0400

    Update 0090-Subsets_II.java

--- 0090-Subsets_II.java
+++ 0090-Subsets_II.java
@@ -1,21 +1,25 @@
-//1 DFS Time O(n * 2^n)(need O(n) to store ), Space: O(n)
+//1 DFS Time O(n * 2^n)(need O(n) to store ), Space: O(n), good solution
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(res,new ArrayList<>(),nums,0);
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        List<Integer> current = new ArrayList<>();
+        helper(nums, 0, res, current);
         return res;
     }
     
-    public void helper(List<List<Integer>> res, List<Integer> ls, int[] nums, int pos) {
-        res.add(new ArrayList<>(ls));
-        for(int i=pos;i<nums.length;i++) {
-            if(i>pos&&nums[i]==nums[i-1]) continue;
-            ls.add(nums[i]);
-            helper(res,ls,nums,i+1);     
-            ls.remove(ls.size()-1);
+    public void helper(int[] nums, int start, List<List<Integer>> res, List<Integer> current) {
+        res.add(new ArrayList<>(current));
+        for(int i = start; i < nums.length; i++) {
+            //如果 nums[i - 1]被放进来了， 下一个recursion的时候start就是i， 
+            //所以判断一下i如果不等于start，nums[i-1]不在current里，如果再加nums[i],则重复之前加了nums[i-1]的结果，所以跳过
+            if(i > start && nums[i] == nums[i - 1]) continue;
+            current.add(nums[i]);
+            helper(nums, i + 1, res, current);     
+            current.remove(current.size() - 1);
         }
-    }
+    }        
 }
 
 //2. DFS Time O(n * 2^n)(need O(n) to store ), Space: O(n)
