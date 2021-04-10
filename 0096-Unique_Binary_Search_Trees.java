4f6c7ae94e267217a58e51a9f8118ad559cbc2b3
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-04-09 22:12:16 -0400

    Update 0096-Unique_Binary_Search_Trees.java

--- 0096-Unique_Binary_Search_Trees.java
+++ 0096-Unique_Binary_Search_Trees.java
@@ -1,4 +1,4 @@
-//dp DP Time:O(n^2) Space:O(n)
+//1.DP Time:O(n^2) Space:O(n)
 //dp[i]表示the number of structurally unique BST's for number i
 //每个数字轮流当root， 左边有j个小于root的数，右边i - 1 - j个数，dp[j]*dp[i - 1 - j]种组合
 class Solution {
@@ -13,4 +13,16 @@ class Solution {
         }
         return dp[n];
     }
+}
+
+//2. Math Time:O(n) Space:O(1)
+class Solution {
+  public int numTrees(int n) {
+    // Note: we should use long here instead of int, otherwise overflow
+    long C = 1;
+    for (int i = 0; i < n; ++i) {
+      C = C * 2 * (2 * i + 1) / (i + 2);
+    }
+    return (int) C;
+  }
 }
