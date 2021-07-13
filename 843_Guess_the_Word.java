f286beba293e82a34dafdf855cdd753181de258e
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-07-12 22:12:15 -0400

    Create 843_Guess_the_Word.java

---
+++ 843_Guess_the_Word.java
@@ -0,0 +1,65 @@
+//1. guess randomly, Time O(n), Space O(n)
+// each round, reduce the possible candidats, the correct word would also have x matches with the previously guessed word, 
+//loop through to find possible candidates
+class Solution {
+    public void findSecretWord(String[] wordlist, Master master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            String guess = wordlist[new Random().nextInt(wordlist.length)];
+            x = master.guess(guess);
+            List<String> wordlist2 = new ArrayList<>();
+            for (String w : wordlist)
+                if (match(guess, w) == x)
+                    wordlist2.add(w);
+            wordlist = wordlist2.toArray(new String[wordlist2.size()]);
+        }
+    }
+    public int match(String a, String b) {
+        int matches = 0;
+        for (int i = 0; i < a.length(); ++i)
+            if (a.charAt(i) == b.charAt(i))
+                matches ++;
+        return matches;
+    }    
+}
+
+
+//2. min max
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Master {
+ *     public int guess(String word) {}
+ * }
+ */
+class Solution {
+    public void findSecretWord(String[] wordlist, Master master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            HashMap<String, Integer> count = new HashMap<>();
+            for (String w1 : wordlist)
+                for (String w2 : wordlist)
+                    if (match(w1, w2) == 0)
+                        count.put(w1, count.getOrDefault(w1 , 0) + 1);
+            String guess = "";
+            int min0 = 100;
+            for (String w : wordlist)
+                if (count.getOrDefault(w, 0) < min0) {
+                    guess = w;
+                    min0 = count.getOrDefault(w, 0);
+                }
+            x = master.guess(guess);
+            List<String> wordlist2 = new ArrayList<String>();
+            for (String w : wordlist)
+                if (match(guess, w) == x)
+                    wordlist2.add(w);
+            wordlist = wordlist2.toArray(new String[0]);
+        }
+    }
+    public int match(String a, String b) {
+        int matches = 0;
+        for (int i = 0; i < a.length(); ++i)
+            if (a.charAt(i) == b.charAt(i))
+                matches ++;
+        return matches;
+    }    
+}
+
