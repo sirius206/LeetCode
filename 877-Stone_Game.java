1fc59c71b64ee729e78cd1a393a5facbfd58a004
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-07-27 22:56:23 -0400

    Create 877-Stone_Game.java

---
+++ 877-Stone_Game.java
@@ -0,0 +1,53 @@
+//1. since number of piles is even, Alice always wins
+//2. 2D DP
+/*
+dp[i][j] means the biggest number of stones you can get more than opponent picking piles in piles[i] ~ piles[j].
+You can first pick piles[i] or piles[j].
+
+If you pick piles[i], your result will be piles[i] - dp[i + 1][j]
+If you pick piles[j], your result will be piles[j] - dp[i][j - 1]
+So we get:
+dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+We start from smaller subarray and then we use that to calculate bigger subarray.
+
+Note that take evens or take odds, it's just an easy strategy to win when the number of stones is even.
+It's not the best solution!
+I didn't find a tricky solution when the number of stones is odd (maybe there is).
+*/
+class Solution {
+    public boolean stoneGame(int[] p) {
+        int n = p.length;
+        int[][] dp  = new int[n][n];
+        for (int i = 0; i < n; i++) dp[i][i] = p[i];
+        for (int d = 1; d < n; d++)
+            for (int i = 0; i < n - d; i++){
+                int j = i + d;
+                dp[i][j] = Math.max(p[i] - dp[i + 1][j], p[j] - dp[i][j - 1]);                
+            }
+
+        return dp[0][n - 1] > 0;
+    }
+}
+
+//2b 2D DP, take turns
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int N = piles.length;
+
+        // dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]].
+        int[][] dp = new int[N+2][N+2];
+        for (int size = 1; size <= N; ++size)
+            for (int i = 0; i + size <= N; ++i) {
+                int j = i + size - 1;
+                int parity = (j + i + N) % 2;  // j - i - N; but +x = -x (mod 2)
+                if (parity == 1)
+                // Alice's turn, max
+                    dp[i+1][j+1] = Math.max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j]);
+                else
+                // Lee's turn, min
+                    dp[i+1][j+1] = Math.min(-piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j]);
+            }
+
+        return dp[1][N] > 0;
+    }
+}
