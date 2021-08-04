8b03cd555a7bcc7014ae060bf42fb41fd0f7e034
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-08-03 22:21:26 -0400

    Update 0100-Same_Tree.java

--- 0100-Same_Tree.java
+++ 0100-Same_Tree.java
@@ -5,9 +5,6 @@ class Solution {
         if (p == null && q == null) return true;
         if ((p == null && q != null) || (p != null && q == null)) return false;
         
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        // or
-        // return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
